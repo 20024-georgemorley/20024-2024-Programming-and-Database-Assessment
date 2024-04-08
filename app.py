a280d9bf1a616f625d543811d48b32036b6fd2d1
20024 <20024@wc.school.nz>
2024-04-08 12:32:05 +1200

    Second commit, minor change

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, request, redirect
-
 import sqlite3
-
 from sqlite3 import Error
 
-# from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
+from flask import Flask, render_template, request, redirect
 
 # This script imports all required modules for the functionality of the code below, for example:
 # render_template - used to render the webpage
@@ -13,7 +11,7 @@ from sqlite3 import Error
 # redirect - imports functionality to redirect user to a different page (usually when error occurs)
 
 app = Flask(__name__)
-# bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 
 DATABASE = ''
 
@@ -35,6 +33,47 @@ def render_home_page():
 @app.route('/dictionary')
 def render_dictionary_page():
     return render_template('dictionary_page.html')
+
+@app.route('/login')
+def render_login_page():
+
+    return render_template('')
+
+@app.route('/signup', methods=['POST','GET'])
+def render_signup_page():
+    if request.method == 'POST':
+        print(request.form)
+        first_name = request.form.get('first_name').title().strip()
+        last_name = request.form.get('last_name').title().strip()
+        email = request.form.get('email').title().strip()
+        user_type = request.form.get('type').title().strip()
+        password = request.form.get('password')
+        password_two = request.form.get('password_two')
+
+        if password != password_two:
+            return redirect('/signup?error=Passwords+do+not+match+try+again')
+
+        if len(password) < 8:
+            return redirect('/signup?error=Password+must+be+at+least+eight+characters+long')
+
+        hashed_password = bcrypt.generate_password_hash(password)
+        con = open_database(DATABASE)
+        query = "INSERT INTO user (type, first_name, last_name, email, password) VALUES (?, ?, ? ,?)"
+        cur = con.cursor()
+
+        try:
+            cur.execute(query, (user_type, first_name, last_name, email, hashed_password))
+        except sqlite3.IntegrityError:
+            con.close()
+            return redirect('/signup?error=Email+is+already+in+use')
+
+        con.commit()
+        con.close()
+
+        return redirect("/login")
+
+    return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run()
