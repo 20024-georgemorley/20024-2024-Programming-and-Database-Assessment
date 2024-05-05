0dd23cdd79446a8eae34da15730510bd6cc0231b
georg <ohakunetabletgeorge@gmail.com>
2024-05-05 18:01:40 +1200

    Eighth commit, major changes to almost all aspects of the program

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ import sqlite3
 from sqlite3 import Error
 
 from flask_bcrypt import Bcrypt
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 # This script imports all required modules for the functionality of the code below, for example:
 # render_template - used to render the webpage
 # Flask - used to import Flask tools.
-# request - imports the ability to 'POST' and 'GET' from a database,. used for users.
+# request - imports the ability to 'POST' and 'GET' from a database, used for users.
 # redirect - imports functionality to redirect user to a different page (usually when error occurs)
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-app.secret_key = "ueuywq9571"
+app.secret_key = "9571"
 
-DATABASE = 'C:/Users/20024/OneDrive - Wellington College/2024 20024 Programming and Database Assessment/Main Project Files/Project/database'
+DATABASE = 'C:/Users/georg/PycharmProjects/20024-2024-Programming-and-Database-Assessment/database'
 
 
-# make sure to add updoot system
+# make sure to add upvote system
 
 
 def open_database(db_file):
@@ -29,11 +29,32 @@ def open_database(db_file):
         return None
 
 
+def is_logged_in():
+    if session.get('email') is None:
+        print('Not logged in.')
+        return False
+    else:
+        print('Logged in.')
+        return True
+
 # Below is where the pages are rendered using an app.route() script and a render_template defined above.
+
 
 @app.route('/')
 def render_home_page():
-    return render_template('home_page.html')
+    return render_template('home_page.html', logged_in=is_logged_in())
+
+
+@app.route('/search')
+def render_search_page():
+    con = open_database(DATABASE)
+    query = 'SELECT maori_name, english_name, category, definition, level, user_id, word_image FROM dictionary WHERE maori_name LIKE "% + search + %" OR english_name LIKE "% + search + %"'
+    cur = con.cursor()
+    cur.execute(query)
+    search_query = cur.fetchall()
+    con.close()
+    print(search_query)
+    return render_template('search_page.html', search=search_query, logged_in=is_logged_in())
 
 
 @app.route('/dictionary')
@@ -45,7 +66,7 @@ def render_dictionary_page():
     dictionary_content = cur.fetchall()
     con.close()
     print(dictionary_content)
-    return render_template('dictionary_page.html', dictionary=dictionary_content)
+    return render_template('dictionary_page.html', dictionary=dictionary_content, logged_in=is_logged_in())
 
 
 @app.route('/dictionary_admin', methods=['POST', 'GET'])
@@ -73,23 +94,65 @@ def render_dictionary_admin():
         con.close()
 
         return redirect("/dictionary")
-    return render_template('dictionary_admin.html')
+    return render_template('dictionary_admin.html', logged_in=is_logged_in())
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def render_login_page():
-    return render_template('login_page.html')
+    if is_logged_in():
+        return redirect("/dictionary")
+    print("Logging In...")
+    if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+        print(email)
+        query = "SELECT user_id, first_name, password FROM users WHERE email = ?"
+        con = open_database(DATABASE)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchone()
+        con.close()
+        print(user_data)
+
+        try:
+            user_id = user_data[0]
+            first_name = user_data[1]
+            db_password = user_data[2]
+        except IndexError:
+            return redirect("/login?error=Please+enter+a+correct+username+or+password")
+
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+or+password+are+invalid")
+
+        session['email'] = email
+        session['first_name'] = first_name
+        session['user_id'] = user_id
+
+        print(session)
+        return redirect('/')
+
+    return render_template('login_page.html', logged_in=is_logged_in())
+
+
+@app.route('/logout')
+def logout_page():
+    print(list(session.keys()))
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect('/?message=Thank+you+for+using+our+website!', logged_in=is_logged_in())
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def render_signup_page():
+    if is_logged_in():
+        return redirect("/dictionary")
     if request.method == 'POST':
         print(request.form)
         print(request.form.get('type'))
         first_name = request.form.get('fname').title().strip()
         last_name = request.form.get('lname').title().strip()
         email = request.form.get('email').title().strip()
-        type = request.form.get('user_type')
+        user_type = request.form.get('user_type')
         password = request.form.get('password')
         password_two = request.form.get('password_two')
 
@@ -99,13 +162,14 @@ def render_signup_page():
         if len(password) < 8:
             return redirect('/signup?error=Password+must+be+at+least+eight+characters+long')
 
+
         hashed_password = bcrypt.generate_password_hash(password)
         con = open_database(DATABASE)
         query = "INSERT INTO users (type, first_name, last_name, email, password) VALUES (?, ?, ? ,? ,?)"
         cur = con.cursor()
 
         try:
-            cur.execute(query, (type, first_name, last_name, email, hashed_password))
+            cur.execute(query, (user_type, first_name, last_name, email, hashed_password))
         except sqlite3.IntegrityError:
             con.close()
             return redirect('/signup?error=Email+is+already+in+use')
@@ -115,7 +179,7 @@ def render_signup_page():
 
         return redirect("/")
 
-    return render_template('signup_page.html')
+    return render_template('signup_page.html', logged_in=is_logged_in())
 
 
 if __name__ == '__main__':
